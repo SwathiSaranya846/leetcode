78efd8559e8b11ea59489a4739ec6bdcc4cd65e6
SwathiSaranya846 <swathisaranya2507@gmail.com>
2026-07-11 09:50:41 +0530

    Time: 8 ms (52.64%), Space: 44.2 MB (48.79%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] s1Count = new int[26];
+        int[] windowCount = new int[26];
+
+        // Count frequencies
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            windowCount[s2.charAt(i) - 'a']++;
+        }
+
+        // Check first window
+        if (matches(s1Count, windowCount)) {
+            return true;
+        }
+
+        // Slide the window
+        for (int i = s1.length(); i < s2.length(); i++) {
+
+            // Add new character
+            windowCount[s2.charAt(i) - 'a']++;
+
+            // Remove old character
+            windowCount[s2.charAt(i - s1.length()) - 'a']--;
+
+            // Compare frequencies
+            if (matches(s1Count, windowCount)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
