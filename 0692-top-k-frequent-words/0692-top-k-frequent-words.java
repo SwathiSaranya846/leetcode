130a83971b8156a7ec261bbdde0b79e44ddcab1a
SwathiSaranya846 <swathisaranya2507@gmail.com>
2026-07-30 23:19:23 +0530

    Time: 9 ms (20.55%), Space: 46.4 MB (70.36%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String,Integer>map=new HashMap<>();
+        for(String word:words){
+            map.put(word,map.getOrDefault(word,0)+1);
+        }
+        List<String>list=new ArrayList<>(map.keySet());
+        Collections.sort(list,(a,b)->{
+            if(map.get(a).equals(map.get(b))){
+                return a.compareTo(b);
+            }
+            return map.get(b)-map.get(a);
+        });
+        return list.subList(0,k);
+        
+    }
+}
